a1f393aea9ef0b1c064c4f7a60655bcf5eb6d8e6
Javier Garcia Muñoz <javidesoft@gmail.com>
2019-10-03 17:14:37 +0200

    Added test to indicate times to repeat a text

--- iteration/src/iteration/repeat_test.go
+++ iteration/src/iteration/repeat_test.go
@@ -2,9 +2,18 @@ package iteration
 
 import "testing"
 
-func TestRepeat(t *testing.T) {
+func TestRepeatDefaultTimes(t *testing.T) {
 	repeated := Repeat("a")
 	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func TestRepeatAmountTimesReturnsTheTextRepeatedTheIndicatedTimes(t *testing.T) {
+	repeated := RepeatAmount("ba", 8)
+	expected := "babababababababa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
