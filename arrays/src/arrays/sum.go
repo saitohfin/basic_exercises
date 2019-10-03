0cdf8d2625b4797e011c5fbbbe3c85b5edc3c73f
Javier Garcia Muñoz <javidesoft@gmail.com>
2019-10-03 18:20:47 +0200

    SumAllTails implemented

--- arrays/src/arrays/sum.go
+++ arrays/src/arrays/sum.go
@@ -11,11 +11,26 @@ func Sum(numbers []int) int {
 	return numberResult
 }
 
+//SumAll do a summatory per each array of numbers
 func SumAll(numbersToSum ...[]int) (sums []int) {
 	amountArrays := len(numbersToSum)
 	sums = make([]int, amountArrays)
 	for position, value := range numbersToSum {
 		sums[position] = Sum(value)
+	}
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	amountArrays := len(numbersToSum)
+	sums = make([]int, amountArrays)
+	for position, value := range numbersToSum {
+		if len(value) == 0 {
+			sums[position] = 0
+		} else {
+			tail := value[1:]
+			sums[position] = Sum(tail)
+		}
 	}
 	return
 }
