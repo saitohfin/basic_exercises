ae207da79f9bae6cb86e0240975aedd3cea6533f
Javier Garcia Muñoz <javidesoft@gmail.com>
2019-10-03 17:55:21 +0200

    Added test for SumAll

--- arrays/src/arrays/sum_test.go
+++ arrays/src/arrays/sum_test.go
@@ -13,3 +13,13 @@ func TestSum2(t *testing.T) {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
 	}
 }
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if got != want {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
