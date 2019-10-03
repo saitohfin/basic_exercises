6f314ee3d86c0bb4f73fb60c11db5ea1253c0fcc
Javier Garcia Muñoz <javidesoft@gmail.com>
2019-10-03 17:55:06 +0200

    Improved Sum to use arrays with any size

--- arrays/src/arrays/sum.go
+++ arrays/src/arrays/sum.go
@@ -1,7 +1,7 @@
 package main
 
 //Sum make a summatory of all numbers of an array
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	numberResult := 0
 
 	for _, number := range numbers {
--- arrays/src/arrays/sum_test.go
+++ arrays/src/arrays/sum_test.go
@@ -2,12 +2,12 @@ package main
 
 import "testing"
 
-func TestSum(t *testing.T) {
+func TestSum2(t *testing.T) {
 
-	numbers := [5]int{1, 2, 3, 4, 5}
+	numbers := []int{1, 2, 3}
 
 	got := Sum(numbers)
-	want := 15
+	want := 6
 
 	if got != want {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
