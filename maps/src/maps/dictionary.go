a6c97981ed36204bc438abe30ccb7a3e37ef664e
Javier Garcia Muñoz <javidesoft@gmail.com>
2019-10-04 15:47:09 +0200

    Check error in the dictionary

--- maps/src/maps/dictionary.go
+++ maps/src/maps/dictionary.go
@@ -1,9 +1,19 @@
 package maps
 
+import "errors"
+
 //Dictionary is a string map
 type Dictionary map[string]string
 
+//ErrNotFound error triggered when a word does not exist in the dictionary
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 //Search find inside a dictionary
-func (d Dictionary) Search(key string) string {
-	return d[key]
+func (d Dictionary) Search(key string) (word string, err error) {
+	word, ok := d[key]
+	if !ok {
+		err = ErrNotFound
+	}
+
+	return word, err
 }
